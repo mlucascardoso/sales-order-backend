142e2c6c9807fe3c4c73fb3c6269897784066f72
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-02-22 19:53:09 -0300

    feat: add cds typer path

--- srv/main.ts
+++ srv/main.ts
@@ -1,3 +1,14 @@
+import { Customer, Customers } from '@models/sales';
+
+const customer: Customer = {
+    email: 'teste@teste.com',
+    firstName: 'Lucas',
+    lastName: 'Cardoso',
+    id: '1234'
+};
+
+const customers: Customers = [customer]
+
 const funcao = (variavel: string) => console.log(variavel);
 
 funcao('123');
