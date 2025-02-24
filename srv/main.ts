a17fe4d35f0079fbec7a0217893ab69f422cc1fb
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-02-24 08:55:53 -0300

    feat: add authorizations in ts code

--- srv/main.ts
+++ srv/main.ts
@@ -2,6 +2,16 @@ import cds, { db, Request, Service } from '@sap/cds';
 import { Customers, Product, Products, SalesOrderHeaders, SalesOrderItem, SalesOrderItems } from '@models/sales';
 
 export default (service: Service) => {
+    service.before('READ', '*', (request: Request) => {
+        if (!request.user.is('read_only_user')) {
+            return request.reject(403, 'Não autorizado');
+        }
+    });
+    service.before(['WRITE', 'DELETE'], '*', (request: Request) => {
+        if (!request.user.is('admin')) {
+            return request.reject(403, 'Não autorizada a escrita/deleção');
+        }
+    });
     service.after('READ', 'Customers', (results: Customers) => {
         results.forEach(customer => {
             if (!customer.email?.includes('@')) {
