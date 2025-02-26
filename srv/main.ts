9b99128da958a82014e2cf46634fdaa92f44a783
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-02-25 22:37:53 -0300

    refactor: move after read customer logic to proper files

--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,7 @@
 import cds, { db, Request, Service } from '@sap/cds';
 import { Customers, Product, Products, SalesOrderHeaders, SalesOrderItem, SalesOrderItems } from '@models/sales';
+import { customerController } from './factories/controllers/customer';
+import { FullRequestParams } from './protocols';
 
 export default (service: Service) => {
     service.before('READ', '*', (request: Request) => {
@@ -12,12 +14,8 @@ export default (service: Service) => {
             return request.reject(403, 'Não autorizada a escrita/deleção');
         }
     });
-    service.after('READ', 'Customers', (results: Customers) => {
-        results.forEach(customer => {
-            if (!customer.email?.includes('@')) {
-                customer.email = `${customer.email}@gmail.com`;
-            }
-        })
+    service.after('READ', 'Customers', (customersList: Customers, request) => {
+        (request as unknown as FullRequestParams<Customers>).results = customerController.afterRead(customersList);
     });
     service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
         const params = request.data;
