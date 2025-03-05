d13c42cac32faa8f7cafc29aa7e3c2a6cede941e
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-03-05 19:04:21 -0300

    refactor: move main service to its own folder

--- srv/routes/main.ts
+++ srv/routes/main.ts
@@ -1,10 +1,10 @@
-import './configs/module-alias';
+import '../configs/module-alias';
 
 import { Request, Service } from '@sap/cds';
 
 import { Customers, SalesOrderHeaders } from '@models/sales';
 
-import { FullRequestParams } from '@/protocols';
+import { FullRequestParams } from '@/routes/protocols';
 import { customerController } from '@/factories/controllers/customer';
 import { salesOrderHeaderController } from '@/factories/controllers/sales-order-header';
 
