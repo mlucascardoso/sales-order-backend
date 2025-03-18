7fb13200ce26230774a1b53c24b08f9b71db4f50
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-03-18 20:48:35 -0300

    feat: create findByCustomerId method in SalesReportRepository and SalesReportRepositoryImpl

--- srv/repositories/sales-report/implementation.ts
+++ srv/repositories/sales-report/implementation.ts
@@ -12,16 +12,28 @@ export class SalesReportRepositoryImpl implements SalesReportRepository {
         subtractedDays.setDate(subtractedDays.getDate() - days);
         const subtractedDaysISOString = subtractedDays.toISOString();
 
-        const sql = SELECT.from('sales.SalesOrderHeaders')
-            .columns(
-                'id as salesOrderId',
-                'totalAmount as salesOrderTotalAmount',
-                'customer.id as customerId',
-                // eslint-disable-next-line quotes
-                `customer.firstName || ' ' || customer.lastName as customerFullName`
-            )
-            .where({ createdAt: { between: subtractedDaysISOString, and: today } });
+        const sql = this.getReportBaseSql().where({ createdAt: { between: subtractedDaysISOString, and: today } });
         const salesReports = await cds.run(sql);
+        return this.mapReportResult(salesReports);
+    }
+
+    public async findByCustomerId(customerId: string): Promise<SalesReportModel[] | null> {
+        const sql = this.getReportBaseSql().where({ customer_id: customerId });
+        const salesReports = await cds.run(sql);
+        return this.mapReportResult(salesReports);
+    }
+
+    private getReportBaseSql(): cds.ql.SELECT<unknown, unknown> {
+        return SELECT.from('sales.SalesOrderHeaders').columns(
+            'id as salesOrderId',
+            'totalAmount as salesOrderTotalAmount',
+            'customer.id as customerId',
+            // eslint-disable-next-line quotes
+            `customer.firstName || ' ' || customer.lastName as customerFullName`
+        );
+    }
+
+    private mapReportResult(salesReports: SalesReportByDays[]): SalesReportModel[] | null {
         if (salesReports.length === 0) {
             return null;
         }
