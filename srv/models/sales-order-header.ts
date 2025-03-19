9d9f4a5fc5d65cd757d730eda6c4f22dcdc41d71
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-03-19 12:09:12 -0300

    feat: add toCreationObject method in SalesOrderHeaderModel

--- srv/models/sales-order-header.ts
+++ srv/models/sales-order-header.ts
@@ -9,6 +9,10 @@ type SalesOrderHeaderProps = {
 
 type SalesOrderHeaderPropsWithoutIdAndTotalAmount = Omit<SalesOrderHeaderProps, 'id' | 'totalAmount'>;
 
+type SalesOrderHeaderPropsWithSnakeCaseCustomerId = Omit<SalesOrderHeaderProps, 'customerId'> & {
+    customer_id: SalesOrderHeaderProps['customerId'];
+};
+
 type CreationPayload = {
     customer_id: SalesOrderHeaderProps['customerId'];
 };
@@ -132,5 +136,14 @@ export class SalesOrderHeaderModel {
 
     public toStringifiedObject(): string {
         return JSON.stringify(this.props);
+    }
+
+    public toCreationObject(): SalesOrderHeaderPropsWithSnakeCaseCustomerId {
+        return {
+            id: this.props.id,
+            customer_id: this.props.customerId,
+            totalAmount: this.calculateDiscount(),
+            items: this.props.items
+        };
     }
 }
