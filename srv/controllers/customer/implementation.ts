18578621058aaa59e60f2754b235b552e505c67f
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-03-23 16:51:49 -0300

    feat: extend BaseController in CustomerController

--- srv/controllers/customer/implementation.ts
+++ srv/controllers/customer/implementation.ts
@@ -2,11 +2,18 @@ import { Customers } from '@models/sales';
 
 import { CustomerController } from '@/controllers/customer/protocols';
 import { CustomerService } from '@/services/customer/protocols';
+import { BaseControllerImpl, BaseControllerResponse } from '@/controllers/base';
 
-export class CustomerControllerImpl implements CustomerController {
-    constructor(private readonly service: CustomerService) {}
+export class CustomerControllerImpl extends BaseControllerImpl implements CustomerController {
+    constructor(private readonly service: CustomerService) {
+        super();
+    }
 
-    public afterRead(customerList: Customers): Customers {
-        return this.service.afterRead(customerList);
+    public afterRead(customerList: Customers): BaseControllerResponse {
+        const result = this.service.afterRead(customerList);
+        if (result.isLeft()) {
+            return this.error(result.value.code, result.value.message);
+        }
+        return this.success(result.value);
     }
 }
