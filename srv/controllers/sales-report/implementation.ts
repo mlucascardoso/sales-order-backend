b86d5042a947e8bcf925693488fbce9c1aa55a3b
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-03-23 17:07:39 -0300

    feat: add Either Monad approach in SalesReportController

--- srv/controllers/sales-report/implementation.ts
+++ srv/controllers/sales-report/implementation.ts
@@ -1,12 +1,19 @@
 import { ExpectedResult as SalesReportByDays } from '@models/db/types/SalesReportByDays';
 import { SalesReportController } from '@/controllers/sales-report/protocols';
 import { SalesReportService } from '@/services/sales-report/protocols';
+import { BaseControllerImpl, BaseControllerResponse } from '@/controllers/base';
 
-export class SalesReportControllerImpl implements SalesReportController {
-    constructor(private readonly service: SalesReportService) {}
+export class SalesReportControllerImpl extends BaseControllerImpl implements SalesReportController {
+    constructor(private readonly service: SalesReportService) {
+        super();
+    }
 
-    public async findByDays(days: number): Promise<SalesReportByDays[]> {
-        return this.service.findByDays(days);
+    public async findByDays(days: number): Promise<BaseControllerResponse> {
+        const result = await this.service.findByDays(days);
+        if (result.isLeft()) {
+            return this.error(result.value.code, result.value.message);
+        }
+        return this.success(result.value);
     }
 
     public async findByCustomerId(customerId: string): Promise<SalesReportByDays[]> {
--- srv/controllers/sales-report/protocols.ts
+++ srv/controllers/sales-report/protocols.ts
@@ -1,6 +1,8 @@
 import { ExpectedResult as SalesReportByDays } from '@models/db/types/SalesReportByDays';
 
+import { BaseControllerResponse } from '@/controllers/base';
+
 export interface SalesReportController {
-    findByDays(days: number): Promise<SalesReportByDays[]>;
+    findByDays(days: number): Promise<BaseControllerResponse>;
     findByCustomerId(customerId: string): Promise<SalesReportByDays[]>;
 }
