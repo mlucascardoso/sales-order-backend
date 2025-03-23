73e4d47646e67e3e4bc7d3a1e22a8542b1263030
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-03-23 19:51:55 -0300

    test: add findByCustomerId method test cases

--- test/unit/services/sales-report/suites.test.ts
+++ test/unit/services/sales-report/suites.test.ts
@@ -21,39 +21,82 @@ const makeSut = (): SutTypes => {
 };
 
 describe('SalesReportService test cases', () => {
-    it('should throw if SalesReportRepository throws', async () => {
-        const { sut, salesReportRepository } = makeSut();
-        vi.spyOn(salesReportRepository, 'findByDays').mockRejectedValueOnce(() => {
-            throw new ServerError('Fake error');
+    describe('method findByDays test cases', () => {
+        it('should return ServerError if SalesReportRepository throws', async () => {
+            const { sut, salesReportRepository } = makeSut();
+            vi.spyOn(salesReportRepository, 'findByDays').mockRejectedValueOnce(() => {
+                throw new ServerError('Fake error');
+            });
+            const result = await sut.findByDays();
+            expect(result.isLeft()).toBeTruthy();
+            expect(result.value).toBeInstanceOf(ServerError);
+            const error = result.value as ServerError;
+            expect(error.code).toBe(500);
+            expect(error.message).toBe('internalServerError');
         });
-        const result = await sut.findByDays();
-        expect(result.isLeft()).toBeTruthy();
-        expect(result.value).toBeInstanceOf(ServerError);
-        const error = result.value as ServerError;
-        expect(error.code).toBe(500);
-        expect(error.message).toBe('internalServerError');
+        it('should return NotFoundError if no records were found for the provided parameters', async () => {
+            const { sut, salesReportRepository } = makeSut();
+            vi.spyOn(salesReportRepository, 'findByDays').mockReturnValueOnce(Promise.resolve(null));
+            const result = await sut.findByDays();
+            expect(result.isLeft()).toBeTruthy();
+            expect(result.value).toBeInstanceOf(NotFoundError);
+            const error = result.value as NotFoundError;
+            expect(error.code).toBe(404);
+            expect(error.message).toBe('Nenhum dado encontrado para os parâmetros informados');
+        });
+        it('should return SalesReport if everything worked as expected', async () => {
+            const { sut } = makeSut();
+            const result = await sut.findByDays();
+            expect(result.isRight()).toBeTruthy();
+            expect(result.value).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        salesOrderTotalAmount: 100,
+                        customerFullName: 'Valid Customer'
+                    })
+                ])
+            );
+        });
     });
-    it('should return NotFoundError if no records were found for the provided parameters', async () => {
-        const { sut, salesReportRepository } = makeSut();
-        vi.spyOn(salesReportRepository, 'findByDays').mockReturnValueOnce(Promise.resolve(null));
-        const result = await sut.findByDays();
-        expect(result.isLeft()).toBeTruthy();
-        expect(result.value).toBeInstanceOf(NotFoundError);
-        const error = result.value as NotFoundError;
-        expect(error.code).toBe(404);
-        expect(error.message).toBe('Nenhum dado encontrado para os parâmetros informados');
-    });
-    it('should return SalesReport if everything worked as expected', async () => {
-        const { sut } = makeSut();
-        const result = await sut.findByDays();
-        expect(result.isRight()).toBeTruthy();
-        expect(result.value).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    salesOrderTotalAmount: 100,
-                    customerFullName: 'Valid Customer'
-                })
-            ])
-        );
+    describe('method findByCustomerId test cases', () => {
+        it('should return ServerError if SalesReportRepository throws', async () => {
+            const { sut, salesReportRepository } = makeSut();
+            vi.spyOn(salesReportRepository, 'findByCustomerId').mockRejectedValueOnce(() => {
+                throw new ServerError('Fake error');
+            });
+            const customerId = crypto.randomUUID();
+            const result = await sut.findByCustomerId(customerId);
+            expect(result.isLeft()).toBeTruthy();
+            expect(result.value).toBeInstanceOf(ServerError);
+            const error = result.value as ServerError;
+            expect(error.code).toBe(500);
+            expect(error.message).toBe('internalServerError');
+        });
+        it('should return NotFoundError if no records were found for the provided parameters', async () => {
+            const { sut, salesReportRepository } = makeSut();
+            vi.spyOn(salesReportRepository, 'findByCustomerId').mockReturnValueOnce(Promise.resolve(null));
+            const customerId = crypto.randomUUID();
+            const result = await sut.findByCustomerId(customerId);
+            expect(result.isLeft()).toBeTruthy();
+            expect(result.value).toBeInstanceOf(NotFoundError);
+            const error = result.value as NotFoundError;
+            expect(error.code).toBe(404);
+            expect(error.message).toBe('Nenhum dado encontrado para os parâmetros informados');
+        });
+        it('should return SalesReportByCustomer if everything worked as expected', async () => {
+            const { sut } = makeSut();
+            const customerId = crypto.randomUUID();
+            const result = await sut.findByCustomerId(customerId);
+            expect(result.isRight()).toBeTruthy();
+            expect(result.value).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        salesOrderTotalAmount: 5,
+                        customerId,
+                        customerFullName: 'Valid Customer'
+                    })
+                ])
+            );
+        });
     });
 });
