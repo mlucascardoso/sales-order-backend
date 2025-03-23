bbf8f61ce4c5a00324900071813cc22a812f394e
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2025-03-23 19:39:09 -0300

    feat: add SalesReportRepositoryStub

---
+++ test/unit/services/sales-report/stubs/sales-report-repository.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { SalesReportModel } from '@/models/sales-report';
+import { SalesReportRepository } from '@/repositories/sales-report';
+
+export class SalesReportRepositoryStub implements SalesReportRepository {
+    public async findByCustomerId(customerId: string): Promise<SalesReportModel[] | null> {
+        const salesOrderId = crypto.randomUUID();
+        const result: SalesReportModel[] = [
+            SalesReportModel.with({
+                salesOrderId,
+                salesOrderTotalAmount: 100,
+                customerId,
+                customerFullName: 'Valid Customer'
+            })
+        ];
+        return result;
+    }
+
+    public async findByDays(days: number): Promise<SalesReportModel[] | null> {
+        const salesOrderId = crypto.randomUUID();
+        const customerId = crypto.randomUUID();
+        const result: SalesReportModel[] = [
+            SalesReportModel.with({
+                salesOrderId,
+                salesOrderTotalAmount: 100,
+                customerId,
+                customerFullName: 'Valid Customer'
+            })
+        ];
+        return result;
+    }
+}
